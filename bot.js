7fd46cc1bdda538f9d4417331101c14c758b35cb
Danila <58362936+gubo4kaa@users.noreply.github.com>
2021-09-15 11:41:13 +0300

    add support button

--- bot.js
+++ bot.js
@@ -6,21 +6,21 @@ const url = 'https://monitor.tis.tatar';
 const dataUri = '/Api/Db/OpenReport/onlineStats';
 const dataUriAll = '/Api/Db/OpenReport/onlineStatsAll';
 
-Number.prototype.toDivide = function() {
-	var int = String(Math.trunc(this));
-	if(int.length <= 3) return int;
-	var space = 0;
-	var number = '';
+Number.prototype.toDivide = function () {
+    var int = String(Math.trunc(this));
+    if (int.length <= 3) return int;
+    var space = 0;
+    var number = '';
 
-	for(var i = int.length - 1; i >= 0; i--) {
-		if(space == 3) {
-			number = ' ' + number;
-			space = 0;
-		}
-		number = int.charAt(i) + number;
-		space++;
-	}
-	return number;
+    for (var i = int.length - 1; i >= 0; i--) {
+        if (space == 3) {
+            number = ' ' + number;
+            space = 0;
+        }
+        number = int.charAt(i) + number;
+        space++;
+    }
+    return number;
 }
 
 console.log('Бот работает')
@@ -30,7 +30,7 @@ const buttons = [
         {
             text: "Татарстан",
             callback_data: "tatar",
-            
+
         },
         {
             text: "Дагестан",
@@ -72,69 +72,69 @@ const buttons = [
 ];
 
 const key_back = [
-    [
-        {
-            text: 'Показать регионы'
-        } 
-    ],
-    [
-        {
-            text: 'Показать регионы'
-        } 
-    ],
+    [{
+        text: 'Показать регионы',
+    }],
+    [{
+        text: 'Тех. поддержка',
+    }],
 
 ];
 
 const fields = [
-    { 
+    {
         code: 'ecg',
         name: 'ЭКГ',
     },
-    { 
+    {
         code: 'holter',
         name: 'XM',
     },
     {
-        code: 'smad', 
+        code: 'smad',
         name: 'СМАД',
     },
 ];
 
 // включаем самого обота
-const bot = new TelegramBot(token, {polling: true});
-
+const bot = new TelegramBot(token, {
+    polling: true
+});
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
-    
+    console.log(msg)
     // отправляем сообщение
-    bot.sendMessage(chatId, 'Выбери регион, в котором хочешь видеть статистику по Единому кардиологу', { // прикрутим клаву
-    reply_markup: {
-        inline_keyboard: buttons
-    }});
-    
+    if (msg.text === 'Тех. поддержка') {
+        bot.sendMessage(chatId, 'Горячая линия: *+78005005149*\nEmail: *mail@tis.tatar*', {
+            parse_mode: 'Markdown'
+        });
+    } else {
+        bot.sendMessage(chatId, 'Выбери регион, в котором хочешь видеть статистику по Единому кардиологу', { // прикрутим клаву
+            reply_markup: {
+                inline_keyboard: buttons
+            }
+        });
+    }
 });
-bot.on('callback_query', (query)=> {
+bot.on('callback_query', (query) => {
     const chatId = query.message.chat.id;
     Promise.all([
-        axios.get(`${url}/${query.data}${dataUri}`), 
-        
-        axios.get(`${url}/${query.data}${dataUriAll}`)
-    ])
-        .then(function([res, resAll]) {
-            
+            axios.get(`${url}/${query.data}${dataUri}`),
+            axios.get(`${url}/${query.data}${dataUriAll}`)
+        ])
+        .then(function ([res, resAll]) {
             const button = _(buttons).flatten().find(['callback_data', query.data]);
-            
             const total = fields.map(field => ({
                 ...field,
-                value:  _.sumBy(res.data.rows, field.code)
+                value: _.sumBy(res.data.rows, field.code)
             }))
-            
+
             const totalAll = fields.map(field => ({
                 ...field,
-                value:  _.sumBy(resAll.data.rows, field.code)
+                value: _.sumBy(resAll.data.rows, field.code)
             }))
-            
+
             bot.sendMessage(chatId, `${button.text} *за сегодня*:\n${total
                 .map(t => `\t${t.name}: ${t.value.toDivide()}`)
                 .join('\n')}`, {
@@ -145,9 +145,6 @@ bot.on('callback_query', (query)=> {
                 reply_markup: {
                     keyboard: key_back
                 }
-                
             });
-
-            
-        });   
+        });
 })
